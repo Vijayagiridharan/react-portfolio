452fb7c7bf6e34145e2403cc1ee81f300b204a9b
Vijayagiridharan <vvijayagiridharan@gmail.com>
2025-04-24 19:52:55 -0400

    changes made

--- src/components/resume/WorkExperience.jsx
+++ src/components/resume/WorkExperience.jsx
@@ -1,7 +1,7 @@
 const WorkExperience = [
     {
         id: 1,
-        title: "Full Stack Developer (Internship)",
+        title: "Full Stack Developer",
         company: "Eva Eco Systems Inc",
         yearsActive: "Feb 2025 – Present",
         information: [
@@ -10,24 +10,35 @@ const WorkExperience = [
     },
     {
         id: 2,
-        title: "Special Events Coordinator (Part-time)",
+        title: "Special Events Supervisor(Part-time)",
         company: "Purdue University Fort Wayne",
         yearsActive: "Mar 2024 – Present",
         location: "Fort Wayne, Indiana (On-site)",
         information: [
+            "Oversee event operations and lead the setup crew for university events, ensuring timely and efficient execution across all event scales.",
             "Coordinating and overseeing the setup of small- and large-scale events on campus, ensuring alignment with event plans and achieving optimal configurations through effective teamwork.",
             "Serving as the primary hospitality liaison for clients, managing all aspects of event execution to ensure seamless and successful outcomes."
         ],
     },
     {
         id: 3,
-        title: "Software Engineer (MEAN Stack)",
-        company: "Cognizant Technology Solutions",
-        yearsActive: "Nov 2022 – Mar 2023",
+        title: "Software Engineer (Backend Engineer)",
+        company: "ImpresERP",
+        yearsActive: "May 2022 - Jun 2023",
         information: [
-            "Built complete web applications using MongoDB, Express.js, Angular, and Node.js stack, implementing secure authentication and payment processing with JWT and Stripe API.",
-            "Optimized backend API design, improving data retrieval by 35%, and enhanced frontend performance through lazy loading and caching, reducing page load times by 50%.",
-             "Established CI/CD pipelines for automated deployments, while effectively managing project workflows with Git, Jira and Agile methodologies to ensure efficient collaboration."
+            "Built secure RESTful APIs with Java, Spring Boot, Spring Security, JWT, and OAuth 2.0 for attendance, billing, and user management; boosted backend performance by 30% through caching and query optimization.",
+            "Integrated MySQL and MongoDB using Spring Data JPA and Hibernate for scalable data management; collaborated with React/Angular teams for smooth API integration and responsive UIs.",
+            "Automated 85% of deployments with Jenkins and Docker; ensured 90%+ test coverage with JUnit 5 and Mockito, and delivered features efficiently using Git, Jira, and Scrum practices."
+        ],
+    },
+    {
+        id: 4,
+        title: "Software Engineer Intern (Backend Engineer)",
+        company: "ImpresERP",
+        yearsActive: "Mar 2022 - Apr 2022",
+        information: [
+            "Constructed Spring Boot APIs for academic scheduling and instituted RBAC with Spring Security, realizing 80%+ test coverage through JUnit 5.",
+            "Orchestrated CRUD workflows with MySQL and Spring Data JPA, coordinated sprint-planning sessions, and logged task progress via Git and Jira.",
         ],
     },
 ];
