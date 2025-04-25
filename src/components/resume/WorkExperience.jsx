ede9d245ae55fce7aa3e948f89dc283233229191
Vijayagiridharan <vvijayagiridharan@gmail.com>
2025-04-24 20:07:12 -0400

    Update work experience

--- src/components/resume/WorkExperience.jsx
+++ src/components/resume/WorkExperience.jsx
@@ -5,7 +5,7 @@ const WorkExperience = [
         company: "Eva Eco Systems Inc",
         yearsActive: "Feb 2025 – Present",
         information: [
-            "Developed the frontend of Park My Ride using React Native, enhancing UI responsiveness and user engagement.", "Integrated multiple React Native screens with Django-based APIs and PostgreSQL for faster data retrieval.", "Improved backend performance and scalability on AWS LightSail by optimizing API response times. Designed a microservices-based backend using Docker and Kubernetes for better system scalability."
+            // "Developed the frontend of Park My Ride using React Native, enhancing UI responsiveness and user engagement.", "Integrated multiple React Native screens with Django-based APIs and PostgreSQL for faster data retrieval.", "Improved backend performance and scalability on AWS LightSail by optimizing API response times. Designed a microservices-based backend using Docker and Kubernetes for better system scalability."
         ],
     },
     {
@@ -15,9 +15,9 @@ const WorkExperience = [
         yearsActive: "Mar 2024 – Present",
         location: "Fort Wayne, Indiana (On-site)",
         information: [
-            "Oversee event operations and lead the setup crew for university events, ensuring timely and efficient execution across all event scales.",
-            "Coordinating and overseeing the setup of small- and large-scale events on campus, ensuring alignment with event plans and achieving optimal configurations through effective teamwork.",
-            "Serving as the primary hospitality liaison for clients, managing all aspects of event execution to ensure seamless and successful outcomes."
+            // "Oversee event operations and lead the setup crew for university events, ensuring timely and efficient execution across all event scales.",
+            // "Coordinating and overseeing the setup of small- and large-scale events on campus, ensuring alignment with event plans and achieving optimal configurations through effective teamwork.",
+            // "Serving as the primary hospitality liaison for clients, managing all aspects of event execution to ensure seamless and successful outcomes."
         ],
     },
     {
@@ -26,9 +26,9 @@ const WorkExperience = [
         company: "ImpresERP",
         yearsActive: "May 2022 - Jun 2023",
         information: [
-            "Built secure RESTful APIs with Java, Spring Boot, Spring Security, JWT, and OAuth 2.0 for attendance, billing, and user management; boosted backend performance by 30% through caching and query optimization.",
-            "Integrated MySQL and MongoDB using Spring Data JPA and Hibernate for scalable data management; collaborated with React/Angular teams for smooth API integration and responsive UIs.",
-            "Automated 85% of deployments with Jenkins and Docker; ensured 90%+ test coverage with JUnit 5 and Mockito, and delivered features efficiently using Git, Jira, and Scrum practices."
+            // "Built secure RESTful APIs with Java, Spring Boot, Spring Security, JWT, and OAuth 2.0 for attendance, billing, and user management; boosted backend performance by 30% through caching and query optimization.",
+            // "Integrated MySQL and MongoDB using Spring Data JPA and Hibernate for scalable data management; collaborated with React/Angular teams for smooth API integration and responsive UIs.",
+            // "Automated 85% of deployments with Jenkins and Docker; ensured 90%+ test coverage with JUnit 5 and Mockito, and delivered features efficiently using Git, Jira, and Scrum practices."
         ],
     },
     {
@@ -37,8 +37,8 @@ const WorkExperience = [
         company: "ImpresERP",
         yearsActive: "Mar 2022 - Apr 2022",
         information: [
-            "Constructed Spring Boot APIs for academic scheduling and instituted RBAC with Spring Security, realizing 80%+ test coverage through JUnit 5.",
-            "Orchestrated CRUD workflows with MySQL and Spring Data JPA, coordinated sprint-planning sessions, and logged task progress via Git and Jira.",
+            // "Constructed Spring Boot APIs for academic scheduling and instituted RBAC with Spring Security, realizing 80%+ test coverage through JUnit 5.",
+            // "Orchestrated CRUD workflows with MySQL and Spring Data JPA, coordinated sprint-planning sessions, and logged task progress via Git and Jira.",
         ],
     },
 ];
